6265b993450e9e991d3207138453f5bad0636ed4
Nicole Henríquez <ale.nicolehs@gmail.com>
2023-05-11 11:22:01 -0400

    Componentes vista HOME

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        <strong>Contact Us</strong>
+      </p>
+      <p>Info</p>
+
+      <div>
+        <img alt="Facebook" />
+        <img alt="Twitter" />
+        <img alt="Instagram" />
+      </div>
+      <p>Copyright Made with LOVE</p>
+    </footer>
+  );
+}
+
+export default Footer;
