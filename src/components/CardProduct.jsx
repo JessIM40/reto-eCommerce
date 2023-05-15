da820898f1b5d19a5027327e1697baaa6a12c3c7
Jhoanna Rosa Mera Dávila <jhoannarosameradavila@gmail.com>
2023-05-15 18:14:38 -0500

    cambiando etiqueta a href por etiqueta h3 y useNavigate para poder hacer el despieqgue

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/prop-types */
 //import React from 'react'
+import { useNavigate  } from 'react-router-dom'
 function CardProduct({producto}) {
+  const navigate = useNavigate();
+
   return (
     <>
             
                 <div className='card-product'>
-                  <a href={`/product/${producto.id}`}>{producto.title}</a>
+                  <h3 onClick={() => navigate(`/product/${producto.id}`)}>{producto.title}</h3>
                     <img src={producto.images[0]} alt=""  className='imagen-producto'/>
                     <p>{producto.description}</p>
                     <strong>${producto.price}</strong>
