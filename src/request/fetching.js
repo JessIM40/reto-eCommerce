783126a5fbb49a07a74b67e1602d8d2876fc0939
Jess <112719297+JessIM40@users.noreply.github.com>
2023-05-14 19:28:52 -0500

    Revert "Main prom jhoa"

--- src/request/fetching.js
+++ src/request/fetching.js
@@ -1,27 +1,18 @@
 async function getProducts(state){
-  const response = await fetch("https://dummyjson.com/products");
-  const data = await response.json();
+    const response = await fetch("https://dummyjson.com/products");
+    const data = await response.json();
+    
+    state(data.products)  
   
-  state(data.products)  
-
-}
+  }
 const uniqueProduct=async (id, state)=>{
-  const response = await fetch(`https://dummyjson.com/products/${id}`);
-  const data = await response.json();
+    const response = await fetch(`https://dummyjson.com/products/${id}`);
+    const data = await response.json();
 
-  state(data)
-  console.log(data)
+    state(data)
+    console.log(data)
 }
-const getProductsByCategory=async( category)=>{
-const response =await fetch(`https://dummyjson.com/products/category/${category}`)
-const data = await response.json();
-// state(data)
-console.log(data)
-
-return data;
-}  
-
-
-export{
-  getProducts, uniqueProduct, getProductsByCategory
-}
+   
+  export{
+    getProducts, uniqueProduct
+  }
